f01808dfee5cfb227eefd91b369ffc5bdfe55e14
Shinsaku Nishino <magnet@MagsBookPro.local>
2025-10-06 11:22:41 +0900

    update

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">  Button
-</button>
     <NuxtRouteAnnouncer
      />
      <NuxtLoadingIndicator />
